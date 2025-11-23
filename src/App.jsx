35b34da9d73127b111d1129d5e612f408d3ca043
ElenaWikoff <elenawikoff@gmail.com>
2025-11-23 14:26:42 -0600

    feat: add css reset and awesomplete

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,42 @@
-import { useState } from 'react'
-import reactLogo from '@/assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useRef, useState } from 'react';
+import reactLogo from '@/assets/react.svg';
+import viteLogo from '/vite.svg';
+import colleges from './data/colleges';
+import Awesomplete from 'awesomplete';
+import 'awesomplete/awesomplete.css';
+// import './App.css';
+
+const initialGeneral = {
+  state: "editing",
+  name: "",
+  email: "",
+  phone: "",
+}
+
+const initialEducation = {
+  state: "editing",
+  school: "",
+  title: "",
+  date: Date.now(),
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+  const inputRef = useRef(null);
+
+  useEffect(() => {
+    if (inputRef.current) {
+      new Awesomplete(inputRef.current, {
+        list: colleges,
+      });
+    }
+  }, [colleges]); // Re-initialize if dataList changes
 
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <label htmlFor="colleges-input">University:</label>
+        <input ref={inputRef} id="colleges-input" type="text" />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
