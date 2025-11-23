3fcc79c480e70a7eef52f3eae625a7545e257c7e
ElenaWikoff <elenawikoff@gmail.com>
2025-11-23 13:52:04 -0600

    feat: add absolute imports

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { resolve } from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      // Example: '@' alias points to the 'src' directory
+      "@": resolve(__dirname, "src"),
+      // You can add more aliases for specific directories, e.g., 'components'
+      // "components": resolve(__dirname, "src/components"),
+    },
+  },
 })
